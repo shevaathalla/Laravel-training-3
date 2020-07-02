a9b2e693060cb290f92f49d9b561f137635c6eda
shevanejiwolop <shevanejiwolop@gmail.com>
2020-07-02 21:36:40 +0700

    add users model and controller

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models;
+
+class UsersController extends Controller
+{
+    public function index()
+    {
+    }
+}
