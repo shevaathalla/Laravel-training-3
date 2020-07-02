1ad0665dce5421254412249d464eb362856453c0
shevanejiwolop <shevanejiwolop@gmail.com>
2020-07-02 21:37:04 +0700

    add users controller

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,5 +9,8 @@ class UsersController extends Controller
 {
     public function index()
     {
+        $users = UsersModel::get_all();
+
+        return view('user.index', compact('items'));
     }
 }
